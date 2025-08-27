b7dc9bcf83b2c1484fe0af933a1346f565d8c47d
ANSHM30 <anshm.cse22@sbjit.edu.in>
2025-08-27 11:38:53 +0530

    Add the following files:

---
+++ server/middleware/auth.js
@@ -0,0 +1,19 @@
+const { verifyAccessToken } = require('../utils/jwt');
+
+function authMiddleware(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader?.startsWith('Bearer ')) {
+    return res.status(401).json({ message: 'No token provided' });
+  }
+
+  const token = authHeader.split(' ')[1];
+  try {
+    const payload = verifyAccessToken(token);
+    req.user = { id: payload.sub };
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: 'Invalid or expired token' });
+  }
+}
+
+module.exports = authMiddleware;
